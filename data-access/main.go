13756fb0454dc5fc3f09fd1b239c486a44a51aaa
JacobLayton <jlayjlay3@gmail.com>
2026-01-16 10:00:00 -0800

    Add albumsByArtist query

--- data-access/main.go
+++ data-access/main.go
@@ -11,6 +11,13 @@ import (
 
 var db *pgxpool.Pool
 
+type Album struct {
+	ID int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 func main() {
 	ctx := context.Background()
 
@@ -40,4 +47,35 @@ func main() {
     }
 
 	fmt.Println("Connected!")
+
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
+
+// albumsByArtist queries for albums that have the specified artist name.
+func albumsByArtist(name string) ([]Album, error) {
+	// an album slice to hold data from recurned rows
+	var albums []Album
+
+	// rows, err := db.Query("Select * FROM album WHERE artist = ?", name)
+	rows, err := db.Query(context.Background(), "SELECT * FROM album WHERE artist = $1", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
