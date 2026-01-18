04c2bcb485a5f09436d92ddcc627f1e993a2a80f
JacobLayton <jlayjlay3@gmail.com>
2026-01-18 15:36:53 -0800

    Create fuzz test, testdata generated

--- fuzz/reverse_test.go
+++ fuzz/reverse_test.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	"testing"
+	"unicode/utf8"
 )
 
-func TestReverse(t *testing.T) {
-	testCases := []struct {
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-        {" ", " "},
-        {"!12345", "54321!"},
-	}
+// func TestReverse(t *testing.T) {
+// 	testCases := []struct {
+// 		in, want string
+// 	}{
+// 		{"Hello, world", "dlrow ,olleH"},
+//         {" ", " "},
+//         {"!12345", "54321!"},
+// 	}
+// 	for _, tc := range testCases {
+// 		rev := Reverse(tc.in)
+// 		if rev != tc.want {
+// 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
+// 		}
+// 	}
+// }
+
+func FuzzReverse(f *testing.F) {
+	testCases := []string{"Hello, world", " ", "!12345"}
 	for _, tc := range testCases {
-		rev := Reverse(tc.in)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q, want %q", rev, tc.want)
-		}
+		f.Add(tc)
 	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev := Reverse(orig)
+		doubleRev := Reverse(rev)
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
